f11720af66d33eb25155ce4f44248dfdf36538d0
m::r <mainstreamer@outlook.com>
2026-01-04 02:30:34 +0000

    feat(coverage): add github workflow and test coverage badge

--- src/Exceptions/FlagsGeneratorException.php
+++ src/Exceptions/FlagsGeneratorException.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 declare(strict_types=1);
 
@@ -9,7 +9,7 @@ class FlagsGeneratorException extends \ErrorException
     private const string MESSAGE = 'Invalid country code given';
 
     public function __construct(
-        string $isoCode,
+        string $isoCode
     ) {
         parent::__construct(message: sprintf('%s %s', self::MESSAGE, $isoCode));
     }
